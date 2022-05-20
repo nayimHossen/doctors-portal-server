580e9e0aae92c978607e50c52473a87d0d5ee329
Nayim hossen <nayimh2015@gmail.com>
2022-05-20 14:47:22 +0600

    booking api implemented

--- index.js
+++ index.js
@@ -16,13 +16,47 @@ async function run() {
     try {
         await client.connect();
         const servicesCollection = client.db('doctors-portal').collection('services');
+        const bookingCollection = client.db('doctors-portal').collection('bookings');
 
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = servicesCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        app.get('/available', async(req, res) => {
+            const date = req.query.date;
+            console.log(date);
+
+            //step 1: get all services
+            const services = await servicesCollection.find().toArray();
+
+            //step 2: get the booking of that day
+            const query = {date: date};
+            const bookings = await bookingCollection.find(query).toArray();
+
+            //step 3: for each service, find bookings for that services
+            services.forEach(service => {
+                const serviceBooking = bookings.filter(b => b.treatment === service.name);
+                const booked = serviceBooking.map(s => s.slot)
+                const available = service.slots.filter(s => !booked.includes(s))
+                service.available = available;
+            })
+            res.send(services);
         })
+
+        app.post('/booking', async(req, res) => {
+            const booking = req.body;
+            const query = {treatment: booking.treatment, date: booking.date, patient: booking.patient}
+            const exists = await bookingCollection.findOne(query);
+
+            if(exists) {
+                return res.send({success: false, booking: exists})
+            }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send({success: true, result});
+        });
 
     }
     finally {
